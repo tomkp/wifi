31e4fce12ac21517a5bcf0bb7ad015eba63827a9
tomkp <tom@tomkp.com>
2016-05-18 20:18:41 +0100

    ...

--- renderer.js
+++ renderer.js
@@ -1,50 +1,6 @@
-const spawn = require('child_process').spawn
-const split = require('split')
-const airport = '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'
-const EventEmitter = require('events').EventEmitter;
-const events = new EventEmitter();
+const wifi = require('./wifi')
 
-
-
-
-
-function start() {
-    
-    let data = {}
-
-    spawn(airport, ['-I'])
-        .stdout
-        .pipe(split())
-        .on('data', function (line) {
-            const info = line.trim().split(': ')
-            if (info.length === 2) {
-                data[info[0]] = info[1]
-            }
-        })
-        .on('end', function () {
-            console.log(`data: ${JSON.stringify(data)}`);
-            if (data.AirPort === 'Off') {
-                events.emit('off')
-                return
-            }
-            const rssi = Number(data.agrCtlRSSI)
-            const noise = Number(data.agrCtlNoise)
-            if (!data.SSID) {
-                events.emit('not-connected')
-                return
-            }
-            events.emit('data', {rssi, noise, ssid: SSID})
-        })
-}
-
-start();
-
-
-
-
-
-
-events
+wifi
     .on('off', () => {
         console.log(`off`);
     })
---
+++ wifi.js
@@ -0,0 +1,43 @@
+const spawn = require('child_process').spawn
+const split = require('split')
+const airport = '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'
+const EventEmitter = require('events').EventEmitter;
+const events = new EventEmitter();
+
+
+const poll = () => {
+
+    let data = {}
+
+    spawn(airport, ['-I'])
+        .stdout
+        .pipe(split())
+        .on('data', function (line) {
+            const info = line.trim().split(': ')
+            if (info.length === 2) {
+                data[info[0]] = info[1]
+            }
+        })
+        .on('end', function () {
+            console.log(`data: ${JSON.stringify(data)}`);
+            if (data.AirPort === 'Off') {
+                events.emit('off')
+                return
+            }
+            const rssi = Number(data.agrCtlRSSI)
+            const noise = Number(data.agrCtlNoise)
+            if (!data.SSID) {
+                events.emit('not-connected')
+                return
+            }
+            events.emit('data', {rssi, noise, ssid: data.SSID})
+        })
+}
+
+
+setInterval(() => {
+    poll();
+}, 1000);
+
+
+module.exports = events
