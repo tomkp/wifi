8b0e766c3e87634f95d338b0299ffdcf058ef880
tomkp <tom@tomkp.com>
2016-06-22 09:14:23 +0100

    ...

--- main.js
+++ main.js
@@ -4,36 +4,40 @@ const BrowserWindow = electron.BrowserWindow;
 const wifi = require('./wifi');
 let mainWindow;
 
-function createWindow () {
-  mainWindow = new BrowserWindow({width: 200, height: 600});
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
-  //mainWindow.webContents.openDevTools()
-  mainWindow.on('closed', function () {
-    mainWindow = null
-  });
-  monitor(mainWindow);
+function createWindow() {
+    mainWindow = new BrowserWindow({width: 200, height: 600});
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+    //mainWindow.webContents.openDevTools()
+    mainWindow.on('closed', function () {
+        //wifi.off('off').off('not-connected').off('data');
+        mainWindow = null
+    });
+    monitor(mainWindow);
 }
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 });
 
 app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow()
-  }
+    if (mainWindow === null) {
+        createWindow()
+    }
 });
 
 const monitor = (win) => {
     wifi.on('off', () => {
-          win.webContents.send('off', 'off');
-        }).on('not-connected', () => {
-          win.webContents.send('not-connected', 'not-connected');
-        }).on('data', ({rssi, noise, ssid, channel}) => {
-          win.webContents.send('data', {rssi, noise, ssid, channel});
-        })
+        if (mainWindow)
+            win.webContents.send('off', 'off');
+    }).on('not-connected', () => {
+        if (mainWindow)
+            win.webContents.send('not-connected', 'not-connected');
+    }).on('data', ({rssi, noise, ssid, channel}) => {
+        if (mainWindow)
+            win.webContents.send('data', {rssi, noise, ssid, channel});
+    })
 };
